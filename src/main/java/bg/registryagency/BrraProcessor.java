1cbcca36811dc9ae19899764afd74a6694e5ccdf
Mihail Stoynov, STY <mihail@sty.bz>
2016-10-12 13:30:04 +0300

    more on the timer

--- src/main/java/bg/registryagency/BrraProcessor.java
+++ src/main/java/bg/registryagency/BrraProcessor.java
@@ -14,51 +14,52 @@ import java.util.zip.ZipInputStream;
 
 public class BrraProcessor {
     public static void main(String[] args) throws Throwable {
-        Stopwatch timer = Stopwatch.createUnstarted();
+        Stopwatch timer = Stopwatch.createStarted();
 
         BrraJaxbUnmarshaller unmarshaller = new BrraJaxbUnmarshaller();
 
-        timer.start();
         URL brra = new URL("http://asfa.bg/brra2016.zip");
-        ZipInputStream zis = new ZipInputStream(new BufferedInputStream(brra.openStream()));
-        java.util.zip.ZipEntry entry;
+        try (ZipInputStream zis = new ZipInputStream(new BufferedInputStream(brra.openStream()))) {
+            java.util.zip.ZipEntry entry;
 
+            // TreeMap, because it's sorted, and we need sorted
+            Map<String, Map<String, BrraCompany>> allCompanies = new TreeMap<>();
 
-        // TreeMap, because it's sorted, and we need sorted
-        Map<String, Map<String, BrraCompany>> allCompanies = new TreeMap<>();
-
-        System.out.print("Processing: ");
-        int countProcessed=0;
-        while ((entry = zis.getNextEntry()) != null) {
+            System.out.print("Processing: ");
+            int countProcessed = 0;
+            while ((entry = zis.getNextEntry()) != null) {
 //            System.out.println("Extracting: " + entry);
-            if(!entry.isDirectory()) {
-                String nameWithoutPath = entry.getName().substring(entry.getName().lastIndexOf(File.separator)+1);
-                System.out.print(nameWithoutPath+", ");
-                Map<String, BrraCompany> dailyCompanies = unmarshaller.unmarshall(new UncloaseableInputStreamProxy(zis));
-                allCompanies.put(nameWithoutPath, dailyCompanies);
-            }
+                if (!entry.isDirectory()) {
+                    String nameWithoutPath = entry.getName().substring(entry.getName().lastIndexOf(File.separator) + 1);
+                    System.out.print(nameWithoutPath + ", ");
+                    Map<String, BrraCompany> dailyCompanies = unmarshaller.unmarshall(new UncloaseableInputStreamProxy(zis));
+                    allCompanies.put(nameWithoutPath, dailyCompanies);
+                }
 
-            if(countProcessed%100==0) {
-                System.out.println();
-                System.out.println("Files processed: "+countProcessed+", for "+timer);
+                if (countProcessed % 100 == 0) {
+                    System.out.println();
+                    System.out.println("Files processed: " + countProcessed + ", for " + timer);
+                }
+                countProcessed++;
             }
-            countProcessed++;
+            System.out.println("Files processed: " + countProcessed + ", for " + timer);
+
+            int numberOfCompaniesBeforeMerge = allCompanies.values().stream().mapToInt(Map::size).sum();
+            System.out.println("numberOfCompaniesBeforeMerge: " + numberOfCompaniesBeforeMerge + ", timer: " + timer);
+
+            Map<String, BrraCompany> allCompaniesFlat = new HashMap<>();
+            allCompanies.entrySet().forEach(
+                    e -> {
+                        System.out.println("Processing map: " + e.getKey());
+                        allCompaniesFlat.putAll(e.getValue());
+                    });
+
+            int numberOfCompaniesAfterMerge = allCompaniesFlat.size();
+            System.out.println("numberOfCompaniesAfterMerge: " + numberOfCompaniesAfterMerge + ", timer: " + timer);
+
+            BrraJsonMarshaller.toJSON(allCompaniesFlat, "brra.json");
+            System.out.println("Finished marshalling to json, timer: " + timer);
         }
-        System.out.println("Files processed: "+countProcessed+", for "+timer);
-
-        int numberOfCompaniesBeforeMerge = allCompanies.values().stream().mapToInt(Map::size).sum();
-        System.out.println("numberOfCompaniesBeforeMerge: "+numberOfCompaniesBeforeMerge);
-
-        Map<String, BrraCompany> allCompaniesFlat = new HashMap<>();
-        allCompanies.entrySet().forEach(
-                e -> {
-                    System.out.println("Processing map: "+e.getKey());
-                    allCompaniesFlat.putAll(e.getValue());
-                });
-
-        int numberOfCompaniesAfterMerge = allCompaniesFlat.size();
-        System.out.println("numberOfCompaniesAfterMerge: "+numberOfCompaniesAfterMerge);
-
-//        BrraJsonMarshaller.toJSON(allCompanieFlat, "brra.json");
+        ;
     }
 }
